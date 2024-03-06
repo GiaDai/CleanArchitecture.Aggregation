e923dc63115dcecdebb357eb4ad18e54fb5714b8
Le Anh Toan <leanhtoan@vietbank.com.vn>
2024-03-06 07:58:02 +0700

    Add enviroment api

--- CleanArchitecture.Aggregation/CleanArchitecture.Aggregation.WebApi/Controllers/MetaController.cs
+++ CleanArchitecture.Aggregation/CleanArchitecture.Aggregation.WebApi/Controllers/MetaController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using System.Diagnostics;
 
 namespace CleanArchitecture.Aggregation.WebApi.Controllers
 {
     public class MetaController : BaseApiController
     {
+        private readonly IWebHostEnvironment _env;
+        public MetaController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         [HttpGet("/info")]
         public ActionResult<string> Info()
         {
@@ -14,6 +21,13 @@ namespace CleanArchitecture.Aggregation.WebApi.Controllers
             var version = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
 
             return Ok($"Version: {version}, Last Updated: {lastUpdate}");
+        }
+
+        // Response Evniroment is production or development
+        [HttpGet("/env")]
+        public ActionResult<string> Env()
+        {
+            return Ok($"Environment is production: {_env.IsProduction()}");
         }
     }
 }
